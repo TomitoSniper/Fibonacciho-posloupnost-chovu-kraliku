232e33d9299ef77572ddc2b8ca59e318a984f6b2
Tomáš Hájek <tomas.hajek2007@seznam.cz>
2025-02-04 09:29:08 +0100

    oiuh

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 // Funkce pro generování Fibonacciho posloupnosti
 function generateFibonacci(n) {
-
+    const divFibonacci = document.getElementById("fibonacci");
+    let a = 1, b = 1, temp;
+    let i = 0;
+    while (i <= n) {
+        const divRabbit = document.createElement("div");
+        let j=0;
+        let str="";
+        while(j<a){
+            str+="🐰";
+            j++;
+        } 
+//console.log(str)
+divRabbit.classList.add("rabbit");
+divRabbit.innerHTML = str;
+divFibonacci.appendChild(divRabbit);
+        temp = a + b;
+        a = b;
+        b = temp;
+        i++;
+    }
 }
 
 
 // Spuštění generování Fibonacciho posloupnosti s 6 prvky
-const n = 6;
-generateFibonacci(n);
+generateFibonacci(15);
